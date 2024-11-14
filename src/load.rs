60ddc8a2aa7755bb064bb749abb9c2e970f60c43
Scott Chase Waggener <chase.waggener@icloud.com>
2024-11-14 21:55:33 +0000

    WIP

--- src/load.rs
+++ src/load.rs
@@ -51,27 +51,85 @@ pub enum LoadError {
     },
 }
 
+/// The order of the channels in the loaded image
 pub enum ChannelOrder {
     First,
     Last,
 }
 
+struct Dimensions {
+    width: usize,
+    height: usize,
+    num_frames: usize,
+}
+
+impl<R: Read+Seek> TryFrom<&mut Decoder<R>> for Dimensions {
+    type Error = TiffError;
+    fn try_from(decoder: &mut Decoder<R>) -> Result<Self, Self::Error> {
+        let (width, height) = decoder.dimensions()?;
+        //let num_frames = decoder.get_tag(tags::NUMBER_OF_FRAMES)?;
+        let num_frames = 2;
+        Ok(Self { width: width as usize, height: height as usize, num_frames: num_frames as usize })
+    }
+}
+
+
+trait LoadFromTiff<T: Clone + num::Zero> {
+    const CHANNEL_ORDER: ChannelOrder;
+    const CHANNELS: usize;
+
+    fn open(path: PathBuf) -> Result<Decoder<BufReader<File>>, LoadError> {
+        let file = BufReader::new(File::open(&path).context(OpenTiffSnafu { path: path.clone() })?);
+        Decoder::new(file).context(ReadTiffSnafu { path })
+    }
+
+    fn preallocate(decoder: &mut Decoder<BufReader<File>>) -> Result<Array4<T>, TiffError> {
+        let dimensions = Dimensions::try_from(decoder)?;
+        let array = Array4::<T>::zeros((
+            dimensions.num_frames,
+            dimensions.height,
+            dimensions.width,
+            Self::CHANNELS));
+        Array::from
+        Ok(array)
+    }
+
+    fn load(path: PathBuf, channel_order: ChannelOrder) -> Result<Array4<T>, LoadError>;
+}
+
 
 struct LoadGray8;
+
+impl LoadFromTiff<u8> for LoadGray8 {
+    fn load(path: PathBuf, channel_order: ChannelOrder) -> Result<Array4<u8>, LoadError> {
+        let decoder = LoadFromTiff::open(path)?;
+        decoder.read_image()
+        let mut array = LoadFromTiff::preallocate(&mut decoder).context(ReadTiffSnafu { path })?;
+
+        let num_frames = decoder.get_tag()
+        let (width, height) = decoder.dimensions().unwrap();
+
+        // Pre-allocate array
+        let mut image_data = Array4::<u8>::zeros((frames, height as usize, width as usize, Self::CHANNELS));
+
+        decoder.read_image()
+
+        let image = decoder.read_image().context(ReadTiffSnafu { path })?;
+        let buffer = image.as_buffer(0);
+
+
+
+        Ok(image.to_luma8().into_raw())
+    }
+}
+
+
 struct LoadGray16;
 struct LoadRgb8;
 struct LoadNormalizedF32;
 
-
-trait Load<T> 
-where T: Clone + num::Zero
-{
-    const CHANNEL_ORDER: ChannelOrder;
-    const CHANNELS: usize;
-
+/*
     fn load(path: PathBuf) -> Result<Array4<T>, LoadError> {
-        let file = BufReader::new(File::open(path).context(OpenTiffSnafu { path })?);
-        let mut decoder = Decoder::new(file).unwrap();
 
         // Determine height, width, number of frames, and color type
         let (width, height) = decoder.dimensions().unwrap();
@@ -98,14 +156,4 @@ where T: Clone + num::Zero
 
 
     }
-
-
-}
-
-
-
-impl Load<f32> for LoadNormalizedF32 {
-    fn load(path: PathBuf) -> Result<Array4<f32>, LoadError> {
-        todo!()
-    }
-}
+*/

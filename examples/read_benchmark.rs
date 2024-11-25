3c2c9a3a32f691e0c59dcb8e56ac7fb499d89df8
Scott Chase Waggener <chase.waggener@icloud.com>
2024-11-25 09:21:17 -0600

    Add inode sort and other benchmark options

--- examples/read_benchmark.rs
+++ examples/read_benchmark.rs
@@ -2,11 +2,15 @@ use clap::Parser;
 use indicatif::{ProgressBar, ProgressFinish, ProgressStyle};
 use ndarray::Array4;
 use rand::seq::SliceRandom;
+use rand::SeedableRng;
 use rayon::prelude::*;
+use rayon::ThreadPoolBuilder;
 use rust_search::SearchBuilder;
 use std::fs::File;
 use std::io::BufReader;
+use std::os::unix::fs::MetadataExt;
 use std::path::PathBuf;
+use std::thread::available_parallelism;
 use tiff::decoder::Decoder;
 use tiff::TiffError;
 
@@ -29,6 +33,20 @@ struct Args {
         default_value_t = 1000
     )]
     iterations: u32,
+
+    #[arg(
+        help = "Thread pool size",
+        short = 't',
+        long = "threads",
+        default_value = None
+    )]
+    threads: Option<usize>,
+
+    #[arg(help = "Random seed", short = 's', long = "seed", default_value_t = 0)]
+    seed: u64,
+
+    #[arg(help = "Sort by inode number", long = "inode", default_value_t = false)]
+    inode: bool,
 }
 
 fn find_tiff_files(dir: &PathBuf) -> impl Iterator<Item = PathBuf> {
@@ -50,7 +68,14 @@ fn find_tiff_files(dir: &PathBuf) -> impl Iterator<Item = PathBuf> {
         .filter(|file| file.extension().unwrap_or_default() == "tiff")
 }
 
-fn open_tiff(path: &PathBuf) -> Result<Vec<usize>, TiffError> {
+/// Sort files by inode number to optimize for sequential reads
+fn sort_by_inode(files: &mut Vec<PathBuf>) {
+    files.sort_by_key(|path| {
+        std::fs::metadata(path).map(|m| m.ino()).unwrap_or(u64::MAX) // Fall back to end of list if metadata read fails
+    });
+}
+
+fn open_tiff(path: &PathBuf, seed: u64) -> Result<Vec<usize>, TiffError> {
     // Open the file and build a decoder
     let file = File::open(path)?;
     let mut decoder = Decoder::new(BufReader::new(file))?;
@@ -59,7 +84,7 @@ fn open_tiff(path: &PathBuf) -> Result<Vec<usize>, TiffError> {
     let metadata = PreprocessingMetadata::try_from(&mut decoder)?;
 
     // Choose a random frame
-    let mut rng = rand::thread_rng();
+    let mut rng = rand::rngs::StdRng::seed_from_u64(seed);
     let frame = metadata
         .num_frames
         .into_iter()
@@ -75,16 +100,32 @@ fn open_tiff(path: &PathBuf) -> Result<Vec<usize>, TiffError> {
     Ok(array.shape().to_owned())
 }
 
-fn open_random_tiff(paths: &[PathBuf]) -> Result<Vec<usize>, TiffError> {
-    let mut rng = rand::thread_rng();
+fn open_random_tiff(paths: &[PathBuf], seed: u64) -> Result<Vec<usize>, TiffError> {
+    let mut rng = rand::rngs::StdRng::seed_from_u64(seed);
     let choice = paths.choose(&mut rng).unwrap().clone();
-    open_tiff(&choice)
+    open_tiff(&choice, seed)
 }
 
 fn main() {
     let args = Args::parse();
+
+    // Set up thread pool
+    ThreadPoolBuilder::new()
+        .num_threads(
+            args.threads
+                .unwrap_or(available_parallelism().unwrap().into()),
+        )
+        .build_global()
+        .unwrap();
+
+    // Find TIFF files
     let source = args.source;
-    let tiff_files = find_tiff_files(&source).collect::<Vec<_>>();
+    let mut tiff_files = find_tiff_files(&source).collect::<Vec<_>>();
+    if args.inode {
+        sort_by_inode(&mut tiff_files);
+    } else {
+        tiff_files.sort();
+    }
     println!("Found {} TIFF files", tiff_files.len());
     if tiff_files.is_empty() {
         println!("No TIFF files found in {}", source.display());
@@ -103,8 +144,14 @@ fn main() {
 
     let size: usize = (0..args.iterations)
         .into_par_iter()
-        .map(|_| {
-            let result = open_random_tiff(&tiff_files).unwrap();
+        .map(|v| {
+            let seed = v as u64 + args.seed;
+            let result = if args.inode {
+                let path = tiff_files[v as usize / args.iterations as usize].clone();
+                open_tiff(&path, seed).unwrap()
+            } else {
+                open_random_tiff(&tiff_files, seed).unwrap()
+            };
             pb.inc(1);
             result.into_iter().reduce(|a, b| a * b).unwrap()
         })

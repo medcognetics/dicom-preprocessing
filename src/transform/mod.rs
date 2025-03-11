a22208d655900d9f8c04b74c7d3f76ad659575f9
Scott Chase Waggener <chase.waggener@icloud.com>
2025-03-11 10:05:34 -0500

    Add trace conversion tool and visualizer (#30)
    * Implment transform and inversion for coordinates
    
    * Style fix
    
    * Reword documentation
    
    * Cleanup
    
    * Support end to end transform
    
    * WIP
    
    * Working processor
    
    * Add trace_hash field
    
    * Implement tests
    
    * Update help
    
    * Update about
    
    * Rename clip -> clamp
    
    * Refactor run()

--- src/transform/mod.rs
+++ src/transform/mod.rs
@@ -8,10 +8,57 @@ pub use pad::*;
 pub use resize::*;
 pub use volume::*;
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct Coord(u32, u32);
+
+impl Coord {
+    pub fn new(x: u32, y: u32) -> Self {
+        Self(x, y)
+    }
+
+    pub fn clamp(&self, width: u32, height: u32) -> Self {
+        Self(self.0.clamp(0, width - 1), self.1.clamp(0, height - 1))
+    }
+
+    pub fn is_in_bounds(&self, width: u32, height: u32) -> bool {
+        self.0 < width && self.1 < height
+    }
+}
+
 pub trait Transform<T> {
+    /// Apply the transform to a target.
     fn apply(&self, target: &T) -> T;
 
+    /// Apply the transform to an iterator of targets.
     fn apply_iter(&self, target: impl Iterator<Item = T>) -> impl Iterator<Item = T> {
         target.map(|t| self.apply(&t))
+    }
+}
+
+impl From<&Coord> for (u32, u32) {
+    fn from(coord: &Coord) -> Self {
+        (coord.0, coord.1)
+    }
+}
+
+impl From<(u32, u32)> for Coord {
+    fn from(tuple: (u32, u32)) -> Self {
+        Coord::new(tuple.0, tuple.1)
+    }
+}
+
+impl Into<(u32, u32)> for Coord {
+    fn into(self) -> (u32, u32) {
+        (self.0, self.1)
+    }
+}
+
+pub trait InvertibleTransform<T>: Transform<T> {
+    /// Invert the transform.
+    fn invert(&self, target: &T) -> T;
+
+    /// Invert the transform over an iterator of targets.
+    fn invert_iter(&self, target: impl Iterator<Item = T>) -> impl Iterator<Item = T> {
+        target.map(|t| self.invert(&t))
     }
 }
